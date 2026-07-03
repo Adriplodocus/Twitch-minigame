d16cd602415bf36eedb204a03f6459da5ec08402
Adrián <adrian08.1991@gmail.com>
2026-07-03 15:15:50 +0200

    feat: bg images on album covers with hover zoom

--- src/album.ts
+++ src/album.ts
@@ -16,12 +16,16 @@ function renderPicker(cards: CardView[]): void {
     const genOwned = genCards.filter((c) => c.quantity > 0).length;
     return `
       <a class="album-cover" href="/album.html?gen=${gen.id}">
-        <p class="album-cover-gen">
-          <span class="album-cover-gen-label">Generación</span>
-          <span class="album-cover-gen-number">${gen.id}</span>
-        </p>
-        <p class="album-cover-region">${gen.region}</p>
-        <span class="album-cover-count">${genOwned}/${genCards.length}</span>
+        <img class="album-cover-bg" src="/album-covers/${gen.id}.webp" alt="" />
+        <span class="album-cover-overlay"></span>
+        <span class="album-cover-content">
+          <p class="album-cover-gen">
+            <span class="album-cover-gen-label">Generación</span>
+            <span class="album-cover-gen-number">${gen.id}</span>
+          </p>
+          <p class="album-cover-region">${gen.region}</p>
+          <span class="album-cover-count">${genOwned}/${genCards.length}</span>
+        </span>
       </a>
     `;
   }).join("");
