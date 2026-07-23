2ba584b70fdb81a504b295b4c5fec316913f1068
Adrián <adrian08.1991@gmail.com>
2026-07-23 13:42:07 +0200

    feat: add trade_offers.marketplace_demand_id column

---
+++ migrations/0025_marketplace_demand_response.sql
@@ -0,0 +1,2 @@
+ALTER TABLE trade_offers ADD COLUMN marketplace_demand_id INTEGER REFERENCES marketplace_offers(id);
+CREATE INDEX idx_trade_offers_marketplace_demand ON trade_offers(marketplace_demand_id);
